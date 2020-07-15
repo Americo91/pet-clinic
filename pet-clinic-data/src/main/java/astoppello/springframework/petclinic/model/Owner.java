3165c5413cae74b9c31544bf74c2f24dbb52c322
astoppello <americo.stoppello@gmail.com>
2020-07-15 16:38:10 +0200

    Create Visit Entity. update Owner #25

--- pet-clinic-data/src/main/java/astoppello/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/astoppello/springframework/petclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package astoppello.springframework.petclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by americo stoppello on 07/07/2020
  */
-public class Owner extends Person{
+public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
